5752a19f13d30e6cb732489311aa560a3146836c
Yue <yue.li.akl@gmail.com>
2018-12-29 20:58:28 +1300

    first reactjs app

---
+++ beginner-001/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * React Element Training
+ *
+ * @author Yue
+ */
+
+?>
+<html>
+<head>
+    <!-- Load React. -->
+    <!-- Note: when deploying, replace "development.js" with "production.min.js". -->
+    <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+</head>
+<body>
+
+    <div id="root"></div>
+    <script src="js/helloworld.js"></script>
+
+</body>
+</html>
---
+++ beginner-001/js/helloworld.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const createElement = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return createElement(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(createElement(LikeButton), domContainer);
